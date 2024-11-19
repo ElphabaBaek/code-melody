646a73a9748709ae01ad463ba578e8ffae6dd912
JaeSeoKim <jackie.kim@healingpaper.com>
2024-11-19 22:17:47 +0900

    페이지 레이아웃 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,14 +20,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} overflow-y-scroll`} >
+      <body className={`${inter.className} flex flex-col overflow-y-scroll min-h-screen`} >
         <Header />
-
-        <main className="flex flex-col items-center m-auto w-full">
+        <main className="flex-1 flex-col items-center m-auto w-full h-full">
           {children}
         </main>
         <Footer />
-        <Analytics />
       </body>
     </html>
   );
